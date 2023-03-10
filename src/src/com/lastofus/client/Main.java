cc4dc3f9f92e9f8569d6fc24b98f7da5704236c3
Jessica G <91770301+Gi-bi@users.noreply.github.com>
2023-03-10 14:41:53 -0800

    Package arrangements and LastofusController added

---
+++ src/src/com/lastofus/client/Main.java
@@ -0,0 +1,12 @@
+package src.com.lastofus.client;
+
+import com.lastofus.appcontroller.LastOfUsAppController;
+
+class Main {
+
+    public static void main(String[] args) {
+//        LastOfUsAppController app = new LastOfUsAppController();
+//        app.execute();
+
+    }
+}
--- src/src/com/main/Main.java
+++
@@ -1,5 +0,0 @@
-package com.main;
-
-class Main {
-
-}
