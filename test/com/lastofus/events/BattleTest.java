7997cebc36789e7b1d9d418eb551b2286710f55d
Justin Chung <justin.chung@tlgcohort.com>
2023-03-14 11:35:34 -0500

    fixed some stuff

--- test/com/lastofus/events/BattleTest.java
+++ test/com/lastofus/events/BattleTest.java
@@ -1,11 +1,16 @@
 package com.lastofus.events;
 
+import com.lastofus.items.Gun;
+import com.lastofus.items.MedKit;
+import com.lastofus.items.Steak;
+import com.lastofus.player.Backpack;
 import com.lastofus.player.Player;
 
 public class BattleTest {
 
     public static void main(String[] args) {
         Player player = new Player("Justin", 100, 100);
+        player.wearBackpack(new Backpack(new Steak(1), new Gun(1), new MedKit(1)));
         Event battle = new Battle(player);
         battle.begin();
     }
